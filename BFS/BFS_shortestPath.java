db5c45fabac30da2717061883065be84d85f30ce
dongyu lin <dongyulin@email.com>
2021-01-06 10:59:40 +0800

    update

---
+++ BFS/BFS_shortestPath.java
@@ -0,0 +1,29 @@
+/**
+ * Return the length of the shortest path between root and target node.
+ */
+int BFS(Node root, Node target) {
+    Queue<Node> queue;  // store all nodes which are waiting to be processed
+    int step = 0;       // number of steps neeeded from root to current node
+    // initialize
+    add root to queue;
+    // BFS
+    while (queue is not empty) {
+        step = step + 1;
+        // iterate the nodes which are already in the queue
+        int size = queue.size();
+        for (int i = 0; i < size; ++i) {
+            Node cur = the first node in queue;
+            return step if cur is target;
+            for (Node next : the neighbors of cur) {
+                add next to queue;
+            }
+            remove the first node from queue;
+        }
+    }
+    return -1;          // there is no path from root to target
+}
+
+/**
+* 在特定问题中执行 BFS 之前确定结点和边缘非常重要。
+* 通常，结点将是实际结点或是状态，而边缘将是实际边缘或可能的转换。
+/
