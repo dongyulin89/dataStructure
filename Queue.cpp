6921bfdf1919169948ea8b78d7e13f1fe8a6b2fd
dongyu lin <dongyulin@email.com>
2021-01-06 10:17:11 +0800

    update

--- Queue.cpp
+++ Queue.cpp
@@ -1,55 +1,51 @@
 #include<iostream>
 #include<vector>
-
 using namespace std;
 
-class MyQueue {
+class MyQueue{
 private:
     // store elements
     vector<int> data;
     // a pointer to indicate the start position
     int p_start;
 public:
-    MyQueue(){
-        p_start = 0;
-    };
-    /* Get the front item from the queue. */
-    int Front() {
-        return data[p_start];
-    };
-
-    /* Checks whether the queue is empty or not. */
-    bool isEmpty() {
-        return p_start >= data.size();
-    };
-
-    /* Insert an element into the queue.
-    Return true if the operation is successful. */
-    bool enQueue(int x) {
+    MyQueue() {p_start=0;}
+    /* Insert an element into the queue. Return true if the operation is successful. */
+    bool enQueue(int x){
         data.push_back(x);
         return true;
-    };
-
-    /* Delete an element from the queue.
-    Return true if the operation is successful. */
-    bool deQueue() {
-        if(isEmpty()) {
+    }
+    /* Delete an element from the queue. Return true if the operation is successful. */
+    bool deQueue(){
+        if(isEmpty()){
             return false;
-        };
+        }
         p_start++;
         return true;
-    };
+    }
+    /* Get the front item from the queue. */
+    int Front(){
+        return data[p_start];
+    }
+    /* Checks whether the queue is empty or not. */
+    bool isEmpty(){
+        return p_start >= data.size();
+    }
 };
 
 int main(){
     MyQueue q;
     q.enQueue(5);
     q.enQueue(3);
-    if(!q.isEmpty())
+    if(!q.isEmpty()){
         cout << q.Front() << endl;
+    }
     q.deQueue();
-    if(!q.isEmpty())
+    if(!q.isEmpty()){
         cout << q.Front() << endl;
-
-    return 0;
+    }
+    q.deQueue();
+    if(!q.isEmpty()){
+        cout << q.Front() << endl;
+    }
 }
